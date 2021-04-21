a351f3baedab2fc61e05928f61a1a782252b1c77
Elana Davis <5271694+elanasparkle@users.noreply.github.com>
2021-04-21 17:44:38 -0400

    Update sprout-clients-dashboard-free.php

--- views/admin/sprout-clients-dashboard-free.php
+++ views/admin/sprout-clients-dashboard-free.php
@@ -78,7 +78,7 @@
 			</div>
 			<div class="last-feature">
 				<h4><?php _e( 'Need help? Or an important feature?' , 'sprout-invoices' ); ?></h4>
-				<p><?php printf( __( "We want to make sure using Sprout Invoices is enjoyable and not a hassle. Sprout Apps has some pretty awesome <a href='%s'>support</a> and a budding <a href='%s'>knowledgebase</a> that will help you get anything resolved." , 'sprout-invoices' ), self::PLUGIN_URL.'/support/', self::PLUGIN_URL.'/support/knowledgebase/' ); ?></p>
+				<p><?php printf( __( "We want to make sure using Sprout Invoices is enjoyable and not a hassle. Sprout Apps has some pretty awesome <a href='%s'>support</a> and a budding <a href='%s'>knowledgebase</a> that will help you get anything resolved." , 'sprout-invoices' ), self::PLUGIN_URL.'support/', self::PLUGIN_URL.'support/knowledgebase/' ); ?></p>
 
 				<p><?php printf( "<a href='https://sproutinvoices.com/support/' target='_blank' class='button'>%s</a>", __( 'Support' , 'sprout-invoices' ) ); ?>&nbsp;<?php printf( "<a href='http://docs.sproutinvoices.com' target='_blank' class='button'>%s</a>", __( 'Documentation' , 'sprout-invoices' ) ); ?></p>
 
